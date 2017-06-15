bf09fa946317f73e4d01fbbce96eecaf61cb79e1
limt <921136839@qq.com>
2017-06-15 15:16:54 +0800

    Signed-off-by: limt <921136839@qq.com>

--- server/public/server.js
+++ server/public/server.js
@@ -1,8 +1,10 @@
-﻿var exec = {
-  test(req, res, next) {
-    console.log("in public")
-    return "success"
-  },
+﻿
+var exec = {
+    createTravel(req, res, next) {
+        var obj = req.query
+        console.log(obj)
+        return "success"
+    },
 
 }
 
@@ -10,12 +12,12 @@
 
 
 module.exports = (req, res, next) => {
-  var action = req.params.action
-  Promise.resolve(action).then(function(result) {
-    return exec[result](req, res, next)
-  }).then(function(result) {
-    res.send(result)
-  }).catch(function(error) {
-    res.status(500).send(error.toString())
-  })
+    var action = req.params.action
+    Promise.resolve(action).then(function(result) {
+        return exec[result](req, res, next)
+    }).then(function(result) {
+        res.send(result)
+    }).catch(function(error) {
+        res.status(500).send(error.toString())
+    })
 }
