dfdcb407788e04cb6dcd95ce249925c7632b4771
limt <921136839@qq.com>
2017-07-05 13:58:27 +0800

    Signed-off-by: limt <921136839@qq.com>

--- db/models/travel_detail.js
+++ db/models/travel_detail.js
@@ -4,12 +4,13 @@ var sequelize = require('../sequelize')
 var travel_detail = sequelize.define(
     'travel_detail', {
         id: {
+            autoIncrement: true
+        },
+        travel_guid: {
             type: Sequelize.INTEGER,
-            autoIncrement: true,
             primaryKey: true
-        },  
-        travel_guid: Sequelize.STRING,
-        index:Sequelize.INTEGER,
+        },
+        index: Sequelize.INTEGER,
         key: Sequelize.STRING,
         value: Sequelize.STRING,
     }, {
