58115006ae3bc2c3884f5234c6b5120d649c7508
master <921136839@qq.com>
2017-07-05 22:50:56 +0800

    Signed-off-by: master <921136839@qq.com>

--- server/public/server.js
+++ server/public/server.js
@@ -67,17 +67,31 @@ var exec = {
             })
             return Promise.all(detailUpsertList)
         }).then(() => {
-          console.log(indexList)
-            // return travel_detail.destroy({
-            //     where: {
-            //         $and: {
-            //             travel_guid: travelID,
-            //             index: {
-            //                 $notIn: indexList
-            //             }
-            //         }
-            //     }
-            // })
+        	return travel_detail.findAll({
+        		where: {
+                    $and: {
+                        travel_guid: travelID,
+                        index: {
+                            $notIn: indexList
+                        }
+                    }
+                }
+        	}).then((result)=>{
+        		if(result.length>0){
+        			return travel_detail.destroy({
+		                where: {
+		                    $and: {
+		                        travel_guid: travelID,
+		                        index: {
+		                            $notIn: indexList
+		                        }
+		                    }
+		                }
+		            }).then(()=>{
+		            	return 'success'
+		            })
+        		}
+        	})        
         })
     }
 }
